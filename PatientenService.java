7fd095910caf9a9b5762661e0850b2047f897ec1
aplicatii <vanessa.iluti@gmail.com>
2025-01-15 15:47:01 +0200

    Add PatientenService

---
+++ PatientenService.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PatientenService {
+    PatientenRepository repo;
+
+    public PatientenService() {this.repo = new PatientenRepository();}
+
+    public void addNewPatienten(int patientenID, String name, Integer alter, String diagnose) {
+        repo.addPatienten(new Patienten( patientenID, name, alter, diagnose));
+    }
+
+    public Patienten getPatientenByID(int id) {
+        return repo.getPatientenById(id);
+    }
+
+    public void deletePatienten(int id) {
+        repo.deletePatienten(id);
+    }
+
+    public void updatePatienten(int id, String name, Integer alter, String diagnose) {
+        Patienten p = repo.getPatientenById(id);
+        p.setName(name);
+        p.setAlter(alter);
+        p.setDiagnose(diagnose);
+        repo.updatePatienten(p);
+    }
+
+    public List<Patienten> getAllPatienten() {
+        return repo.getAllPatienten();
+    }
+
+
+}
